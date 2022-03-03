08c0e988d26f51d9dd846fa4ff25cdc21276f543
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-03 09:11:51 +0000

    ..

--- p7final.c
+++ p7final.c
@@ -2,22 +2,43 @@
 struct _fraction
 {
 int num,den;
-};
-typedef struct_fraction,fraction
+}
+typedef Fraction;
 int gcd(int a,int b)
 {
   int gcd(int a,int b);
   int t;
-    while (b!=0)
+    while (b !=0)
       {
         t = b;
-        b = a % b;
+        b = a%b;
         a = t;
         }
     return a;
 }
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+Fraction add_fractions(Fraction f1, Fraction f2)
+{
+  Fraction sum;
+  sum.num=f1.num*f2.den+f2.num*f1.den;
+  sum.den=f1.den*f2.den;
+  int g=gcd(sum.num,sum.den);
+  sum.num=sum.num/g;
+  sum.den=sum.den/g;
+  return sum;
+}
 int main()
 {
-  printf("%d\n",gcd(16,24));
+  Fraction f1,f2,sum;
+  f1=input_fraction();
+  f2=input_fraction();
+  sum=add_fractions(f1,f2);
+  printf("The fraction is %d/%d\n",sum.num,sum.den);
   return 0;
 }
