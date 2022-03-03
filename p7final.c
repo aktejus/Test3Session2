03a1362a69cc8321538b250f53be862681d88049
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-03 08:48:54 +0000

    ..

--- p7final.c
+++ p7final.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef struct_fraction,fraction
+int gcd(int a,int b)
+{
+  int gcd(int a,int b);
+  int t;
+    while (b!=0)
+      {
+        t = b;
+        b = a % b;
+        a = t;
+        }
+    return a;
+}
+int main()
+{
+  printf("%d\n",gcd(16,24));
+  return 0;
+}
