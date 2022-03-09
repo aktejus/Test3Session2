4a16006c6ed4ce9dfecd91b3b1e032483af5b8b1
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-09 03:44:23 +0000

    P1 done

--- p1final.c
+++ p1final.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+void input(int *num1,int *den1,int *num2,int *den2)
+{
+  printf("Enter the two fractions\n");
+  scanf("%d%d %d%d",num1,den1,num2,den2);
+}
+void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
+{
+  *den3=den1*den2;
+  *num3=(num1*den2)+(num2*den1);
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+  printf("The sum of %d/%d + %d/%d is %d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int a,b,c,d,x,y;
+  input(&a,&b,&c,&d);
+  add(a,b,c,d,&x,&y);
+  output(a,b,c,d,x,y);
+  return 0;
+}
