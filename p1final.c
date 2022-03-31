9a50754def778af6c9de49265ed143736951b962
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-03-31 18:33:12 +0000

    ..

--- p1final.c
+++ p1final.c
@@ -6,8 +6,8 @@ void input(int *num1,int *den1,int *num2,int *den2)
 }
 void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
 {
-  *den3=den1*den2;
-  *num3=(num1*den2)+(num2*den1);
+  *den3= den1*den2;
+  *num3= (num1*den2)+(num2*den1);
 }
 void output(int num1,int den1,int num2,int den2,int num3,int den3)
 {
